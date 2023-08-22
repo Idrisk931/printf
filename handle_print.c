626f11840b4fe6d156d572ee1ec75d6d503944cb
Idrisk931 <fortunejeremy14@gmail.com>
2023-08-22 16:14:23 -0700

    handle_print

--- handle_print.c
+++ handle_print.c
@@ -1,65 +1,62 @@
 #include "main.h"
 
 /**
- * handle_format - Handles printing for a specific format specifier.
- * @fmt: Format specifier to be handled.
+ * handle_print - Prints arguments based on their types.
+ * @fmt: Formatted string containing the print arguments.
+ * @ind: Pointer to the index in the format string.
  * @list: List of arguments to be printed.
- * @buffer: Buffer array to handle print.
- * @flags: Active flags.
- * @width: Width specification.
- * @precision: Precision specification.
- * @size: Size specifier.
- *
- * Return: Number of characters printed or -1 on error.
- */
-static int handle_format(const char fmt, va_list list, char buffer[],
-			int flags, int width, int precision, int size)
-{
-
-}
-
-/**
- * handle_unknown - Handles unknown format specifiers and width handling.
- * @fmt: Format specifier to be handled.
- * @ind: Pointer to the current index in the format string.
- * @width: Width specification.
- *
- * Return: Number of characters written.
- */
-static int handle_unknown(const char fmt, int *ind, int width)
-{
-
-}
-
-/**
- * handle_print - Main printing function to process the format string.
- * @fmt: Formatted string in which to print the arguments.
- * @ind: Pointer to the current index in the format string.
- * @list: List of arguments to be printed.
- * @buffer: Buffer array to handle print.
- * @flags: Active flags.
- * @width: Width specification.
- * @precision: Precision specification.
- * @size: Size specifier.
- *
- * Return: Number of characters printed or -1 on error.
+ * @buffer: Buffer array for handling print.
+ * @flags: Active formatting flags.
+ * @width: Width specifier for formatting.
+ * @precision: Precision specification for formatting.
+ * @size: Size specifier for formatting.
+ * Return: Number of characters printed or -1 on failure.
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-			int flags, int width, int precision, int size)
+		int flags, int width, int precision, int size)
 {
-	const char curr_fmt = fmt[*ind];
-	(*ind)++;
+	int i, unknow_len = 0, printed_chars = -1;
 
-	if (curr_fmt == '\0')
-	{
-		return (-1);
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
+
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+
+		if (fmt[*ind] == fmt_types[i].fmt)
+		{
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+		}
+
+	if (fmt_types[i].fmt == '\0')
+
+		if (fmt[*ind] == '\0')
+		{
+			return (-1);
+		}
+		unknow_len += write(1, "%%", 1);
+		if (fmt[*ind - 1] == ' ')
+			unknow_len += write(1, " ", 1);
+		else if (width)
+
+			--(*ind);
+			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
+			{
+				--(*ind);
+			}
+			if (fmt[*ind] == ' ')
+			{
+				--(*ind);
+			}
+			return (1);
+		}
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
 
-	if (curr_fmt == '%')
-	{
-		return (handle_unknown fmt[*ind], ind, width);
-	}
-
-	return (handle_format(curr_fmt, list, buffer, flags, width, precision, size));
+	return (printed_chars);
 }
-
