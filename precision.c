bb13332be899f3780c7f1307cf2687a55a5dd3b4
Idrisk931 <fortunejeremy14@gmail.com>
2023-08-22 18:56:50 -0700

    precision.c

--- precision.c
+++ precision.c
@@ -13,9 +13,10 @@
  * @list: The list of arguments for the formatted string.
  *
  * Return: The extracted precision value.
+ *
  */
-int get_precision(const char *format, int *i, va_list list)
-{
+	int get_precision(const char *format, int *i, va_list list)
+	{
 	int curr_i = *i + 1;
 	int precision = -1;
 
